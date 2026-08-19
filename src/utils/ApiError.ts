ab92aeee0185287d3e20c75f5fe01ed287eb8a0b
TechAsish1212 <gitasish1212@gmail.com>
2026-08-20 00:05:39 +0530

    feat: add error handler

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,12 @@
+class ApiError extends Error {
+  statusCode: number;
+  status: "fail" | "error";
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ApiError;
